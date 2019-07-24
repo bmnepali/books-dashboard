385085fd9b893a14a6856fbd3a69e33610c8eb4d
Buddha Man Nepali <bmnepali980@gmail.com>
2019-07-24 11:19:57 +0545

    BDA: Adds files for the starter project

---
+++ src/index.js
@@ -0,0 +1,15 @@
+/* global document */
+
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+
+import App from './App';
+import store from './store';
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('books-dashboard-app')
+);
