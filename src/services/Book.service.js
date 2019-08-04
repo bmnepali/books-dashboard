12c2b7599465077858a5d152e093e52cc7e66da3
Buddha Man Nepali <bmnepali980@gmail.com>
2019-08-04 19:13:22 +0545

    BDA: Fixes books api path, which was left incorrect during testing

--- src/services/Book.service.js
+++ src/services/Book.service.js
@@ -2,7 +2,7 @@ import http from '../utilities/api';
 
 const BooksService = {
   getBooks(category) {
-    return http.get('lists.json?lis-name=' + category + '&api-key=6ad84e249d054efeaefe1abb8f89df5b').then(
+    return http.get('lists.json?list-name=' + category + '&api-key=6ad84e249d054efeaefe1abb8f89df5b').then(
       response => Promise.resolve(response),
       error => {
         return Promise.reject(error);
