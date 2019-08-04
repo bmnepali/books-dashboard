aec5eb97689417a742f899bc476eefe32439ad10
Buddha Man Nepali <bmnepali980@gmail.com>
2019-08-04 18:22:59 +0545

    BDA: Adds more test cases for reducers

--- src/__tests__/reducers/book.spec.js
+++ src/__tests__/reducers/book.spec.js
@@ -1,7 +1,47 @@
-import booksReducer from '../../reducers/book';
+import booksReducer, {initialBookState} from '../../reducers/book';
+import { BOOKS_LOAD, BOOKS_LOADED, BOOKS_ERROR, CHANGE_CATEGORY } from '../../constants/BookActionTypes';
 
 describe('Books Reducer', () => {
   it('returns the initial state', () => {
     expect(booksReducer(undefined, {})).toMatchSnapshot();
+  });
+
+  it('returns updated state on loading books', () => {
+    const reducer = booksReducer(initialBookState, { type: BOOKS_LOAD });
+
+    expect(reducer.isLoading).toBe(true);
+    expect(reducer.books.length).toBe(0);
+  });
+
+  it('returns updated state on books are loaded', () => {
+    const reducer = booksReducer(initialBookState, {
+      type: BOOKS_LOADED,
+      payload: [ {}, {}]
+    });
+
+    expect(reducer.isLoading).toBe(false);
+    expect(reducer.books.length).toBe(2);
+  });
+
+  it('returns updated state on error', () => {
+    const reducer = booksReducer(initialBookState, {
+      type: BOOKS_ERROR,
+      payload: 'This is the sample error while loading books'
+    });
+
+    expect(reducer.isLoading).toBe(false);
+    expect(reducer.books.length).toBe(0);
+    expect(reducer.error).toBe('This is the sample error while loading books');
+  });
+
+  it('returns updated state on category change', () => {
+    const reducer = booksReducer(initialBookState, {
+      type: CHANGE_CATEGORY,
+      payload: 'changed-category-name'
+    });
+
+    expect(reducer.isLoading).toBe(false);
+    expect(reducer.books.length).toBe(0);
+    expect(reducer.category).toBe('changed-category-name');
   });
 });
